af30c46611d1b02554a04ce9a4ba6e644439a59e
dollyn <dollyn.sun@gmail.com>
2018-02-09 01:03:20 +0800

    support huge files

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import fs = require('fs');
+import readline = require('readline')
+import stream = require('stream')
 import * as vscode from 'vscode';
+import { resolve } from 'path';
 
 const ruleMap = new Map<string, vscode.CommentRule>();
 
@@ -12,12 +15,19 @@ let cLikeRule = {
     blockComment: ["/*", "*/"] as vscode.CharacterPair
 };
 
+let sqlRule = {
+    lineComment: "#"
+}
+
 ruleMap.set("js", cLikeRule);
 ruleMap.set("java", cLikeRule);
+ruleMap.set("kt", cLikeRule);
 ruleMap.set("go", cLikeRule);
 ruleMap.set("ts", cLikeRule);
+ruleMap.set("sql", sqlRule)
 
 
+let channel;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -26,7 +36,7 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "line-counter" is now active!');
 
-    let channel = vscode.window.createOutputChannel("Line Counter");
+    channel = vscode.window.createOutputChannel("Line Counter");
     
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -58,30 +68,33 @@ export function activate(context: vscode.ExtensionContext) {
             let excludesCfg = cfg.get<string[]>("excludes");
             let excludes = `{${excludesCfg.join(',')}}`;
 
+            let res: LineCount = {all: 0, comment: 0, blank: 0};
+            let counter = Promise.resolve(res)
+
             vscode.workspace.findFiles(includes, excludes, 0).then(value => {
-                let result: LineCount = {all: 0, commnet: 0, blank: 0};
-                console.log(`countting ${value} ...`);
+                console.log(`counting ${value} ...`);
                 for (let _i = 0; _i < value.length; _i++) {
                     let file = value[_i];      
-                    console.log('counting ' + file.path + "...");       
-                    let resultOfFile = doCount(file.fsPath);       
-                    result.all += resultOfFile.all;
-                    result.commnet += resultOfFile.commnet;
-                    result.blank += resultOfFile.blank;             
+                    console.log('counting ' + file.path + "...");    
+                    counter = counter.then(res => {
+                        return count(file.path, res)
+                    })             
                 }
-                channel.show();
-                let end = Date.now();
-                let time = end - start;
-                channel.appendLine(`Countting took ${time} ms. The result is: `);
-                channel.appendLine(`    total:\t${result.all}`);
-                channel.appendLine(`    code:\t${result.all - result.commnet - result.blank}`);
-                channel.appendLine(`    comment:\t${result.commnet}`);
-                channel.appendLine(`    blank:\t${result.blank}`);
-                console.timeEnd("count");
+                counter.then(result => {
+                    channel.show();
+                    let end = Date.now();
+                    let time = end - start;
+                    channel.appendLine(`Countting took ${time} ms. The result is: `);
+                    channel.appendLine(`    total:\t${result.all.toLocaleString()}`);
+                    channel.appendLine(`    code:\t${(result.all - result.comment - result.blank).toLocaleString()}`);
+                    channel.appendLine(`    comment:\t${result.comment.toLocaleString()}`);
+                    channel.appendLine(`    blank:\t${result.blank.toLocaleString()}`);
+                    console.timeEnd("count");
+                })                               
             });           
         } else {
             channel.show();
-            channel.appendLine('no workspace, countting cancled...');
+            channel.appendLine('no workspace, counting canceled...');
         }
     });
     
@@ -95,53 +108,69 @@ export function deactivate() {
 
 interface LineCount {
     all: number;
-    commnet: number;
+    comment: number;
     blank: number;
 }
 
-export function doCount(path: string) {
-    let ext = getExt(path);
-    let commentRule = ruleMap.get(ext);
-    let result: LineCount = {all: 0, commnet: 0, blank: 0};
-
-    let content = fs.readFileSync(path, 'utf-8');
+function count(path: string, countObj: LineCount): Promise<LineCount> {
+    console.log("counting ${path}")
+    return new Promise((resolve, reject) => {
+        let ext = getExt(path);
+        let commentRule = ruleMap.get(ext);
+        let result: LineCount = {all: 0, comment: 0, blank: 0};
     
-    let blockComment = false;
-    content.split('\n').forEach(line => {
-        let trimed = line.trim();
-        result.all++;
-        if (trimed.length == 0) {
-            result.blank++;
-            if (blockComment) {
-                result.commnet++;
+        var instream = fs.createReadStream(path);
+        var rl = readline.createInterface({
+            input: instream
+        })
+    
+        let blockComment = false
+        
+        rl.on('line', function(line){
+            let trimed = line.trim();
+            result.all++;
+            if (result.all % 10000000 == 0) {
+                channel.appendLine(`still counting ${path}, current line: ${result.all.toLocaleString()}`)
             }
-        } else if (commentRule) {
-            if (trimed.startsWith(commentRule.lineComment)) {
-                result.commnet++;
-            } else {
-                if (commentRule.blockComment) {
-                    if (blockComment) {
-                        result.commnet++;
-                        // end of block comment?
-                        if (line.indexOf(commentRule.blockComment[1]) >= 0) {
-                            blockComment = false;
-                        }
-                    } else {
-                        if (line.indexOf(commentRule.blockComment[0]) >= 0) {
-                            blockComment = true;
-                            result.commnet++;
+            if (trimed.length == 0) {
+                result.blank++;
+                if (blockComment) {
+                    result.comment++;
+                }
+            } else if (commentRule) {
+                if (trimed.startsWith(commentRule.lineComment)) {
+                    result.comment++;
+                } else {
+                    if (commentRule.blockComment) {
+                        if (blockComment) {
+                            result.comment++;
+                            // end of block comment?
                             if (line.indexOf(commentRule.blockComment[1]) >= 0) {
                                 blockComment = false;
                             }
+                        } else {
+                            if (line.indexOf(commentRule.blockComment[0]) >= 0) {
+                                blockComment = true;
+                                result.comment++;
+                                if (line.indexOf(commentRule.blockComment[1]) >= 0) {
+                                    blockComment = false;
+                                }
+                            }
                         }
-                        
                     }
                 }
+            } 
+        })
+        rl.on('close', () => {
+            countObj.all += result.all
+            countObj.blank += result.blank
+            countObj.comment += result.comment
+            if (result.all >= 10000000) {
+                channel.appendLine(`counting ${path} finished.`)
             }
-        }
+            resolve(countObj)
+        })
     });
-
-    return result;
 }
 
 function getExt(path: string) {
